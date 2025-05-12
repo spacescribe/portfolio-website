cd5de0daab45b93a994280e4c73cf0fd42e02060
NAC043 <nandiniac.211it043@nitk.edu.in>
2025-05-12 23:58:52 +0530

    Added docs to the repo folder

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),],
-  base: '/portfolio-website/', // e.g. '/portfolio/'
+  base: '/portfo/', // e.g. '/portfolio/'
   build: {
     outDir: 'docs', // 👈 this is the key change
   },
